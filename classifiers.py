f7e6435668ad3c5a686d52354b2de786452c0ae6
Littleote <david.candelar@gmail.com>
2024-03-02 19:02:10 +0100

    Simplified classifier

--- classifiers.py
+++ classifiers.py
@@ -6,78 +6,6 @@ from sklearn.ensemble import RandomForestClassifier
 from utils import toNumpyArray
 
 
-def applyNaiveBayes(X_train, y_train, X_test, **kwargs):
-    """
-    Task: Given some features train a Naive Bayes classifier
-          and return its predictions over a test set
-    Input; X_train -> Train features
-           y_train -> Train_labels
-           X_test -> Test features
-    Output: y_predict -> Predictions over the test set
-    """
-    trainArray = toNumpyArray(X_train)
-    testArray = toNumpyArray(X_test)
-
-    clf = MultinomialNB(**kwargs)
-    clf.fit(trainArray, y_train)
-    y_predict = clf.predict(testArray)
-    return y_predict
-
-
-def applySupportVectorMachine(X_train, y_train, X_test, **kwargs):
-    """
-    Task: Given some features train an SVM classifier
-          and return its predictions over a test set
-    Input; X_train -> Train features
-           y_train -> Train_labels
-           X_test -> Test features
-    Output: y_predict -> Predictions over the test set
-    """
-    trainArray = toNumpyArray(X_train)
-    testArray = toNumpyArray(X_test)
-
-    clf = SVC(**kwargs)
-    clf.fit(trainArray, y_train)
-    y_predict = clf.predict(testArray)
-    return y_predict
-
-
-def applyKNearestNeighbours(X_train, y_train, X_test, **kwargs):
-    """
-    Task: Given some features train a nearest neighbours classifier
-          and return its predictions over a test set
-    Input; X_train -> Train features
-           y_train -> Train_labels
-           X_test -> Test features
-    Output: y_predict -> Predictions over the test set
-    """
-    trainArray = toNumpyArray(X_train)
-    testArray = toNumpyArray(X_test)
-
-    clf = KNeighborsClassifier(**kwargs)
-    clf.fit(trainArray, y_train)
-    y_predict = clf.predict(testArray)
-    return y_predict
-
-
-def applyRandomForest(X_train, y_train, X_test, **kwargs):
-    """
-    Task: Given some features train a nearest neighbours classifier
-          and return its predictions over a test set
-    Input; X_train -> Train features
-           y_train -> Train_labels
-           X_test -> Test features
-    Output: y_predict -> Predictions over the test set
-    """
-    trainArray = toNumpyArray(X_train)
-    testArray = toNumpyArray(X_test)
-
-    clf = RandomForestClassifier(**kwargs)
-    clf.fit(trainArray, y_train)
-    y_predict = clf.predict(testArray)
-    return y_predict
-
-
 def applyClassifier(classifier, X_train, y_train, X_test, **kwargs):
     """
     Task: Given some features train some classifier
@@ -88,13 +16,19 @@ def applyClassifier(classifier, X_train, y_train, X_test, **kwargs):
            X_test -> Test features
     Output: y_predict -> Predictions over the test set
     """
-    fun = CLASSIFIERS.get(classifier, applyNaiveBayes)
-    return fun(X_train, y_train, X_test, **kwargs)
+    cls = CLASSIFIERS.get(classifier, MultinomialNB)
+    trainArray = toNumpyArray(X_train)
+    testArray = toNumpyArray(X_test)
+
+    clf = cls(**kwargs)
+    clf.fit(trainArray, y_train)
+    y_predict = clf.predict(testArray)
+    return y_predict
 
 
 CLASSIFIERS = dict(
-    NB=applyNaiveBayes,
-    SVM=applySupportVectorMachine,
-    KNN=applyKNearestNeighbours,
-    RF=applyRandomForest,
+    NB=MultinomialNB,
+    SVM=SVC,
+    KNN=KNeighborsClassifier,
+    RF=RandomForestClassifier,
 )
